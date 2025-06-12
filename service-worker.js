e58ef4f6bd2c3ce43b24268dcdaa5914e1fff49f
Mii <mutron.xiaomii@gmail.com>
2025-06-13 03:22:28 +0800

    Ver--2.0 update PWA

--- service-worker.js
+++ service-worker.js
@@ -3,12 +3,16 @@ const urlsToCache = [
   "/",
   "/index.html",
   "/main.js", // 你的 JS 檔名
-  "/style.css", // 你的 CSS 檔名
-  "/images/user/fishing1.png",
-  "/images/user/fishing2.png",
-  "/images/user/fishing3.png",
-  "/images/user/fishing4.png",
-  // ...還有所有圖片、icon
+  "/main.css", // 你的 CSS 檔名
+  "/images/index/index1.jpg",
+  "/images/index/backpackbg.jpg",
+  "/images/icons/coin.png",
+  "/images/icons/equip.png",
+  "/images/icons/backpack.png",
+  "/images/icons/shop.png",
+  // 魚
+  "/images/fishes/fish1.png",
+  "/images/fishes/fish2.png",
 ];
 
 self.addEventListener("install", (event) => {
